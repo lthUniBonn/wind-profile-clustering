876937345de9d82e756e73422e9b4020952800e7
lthUniBonn <lthimm@uni-bonn.de>
2021-06-11 12:37:40 +0200

    add chain compatibility (npcs) and chain visualisation on PC analysis

--- principal_component_analysis.py
+++ principal_component_analysis.py
@@ -4,7 +4,7 @@ import numpy as np
 
 import matplotlib as mpl
 
-from config import plots_interactive, result_dir, data_info
+from config import plots_interactive, result_dir, data_info, n_pcs
 from read_requested_data import get_wind_data
 
 if not plots_interactive:
@@ -101,6 +101,32 @@ def plot_mean_and_pc_profiles(altitudes, var, get_profile, plot_info=""):
     if not plots_interactive: plt.savefig(result_dir + 'pc_mean_and_pc_profiles' + plot_info + '.pdf')
 
 
+def plot_pc_profiles(altitudes, get_profile, plot_info=""):
+    x_label = r"$\tilde{v}$ [-]"
+
+    # Plot PCs
+    for i_pc in range(n_pcs):
+        # Plot profiles.
+        cm = 1/2.54
+        fig = plt.figure(figsize=(15*cm, 20*cm))
+        ax = fig.add_subplot(111)
+
+        ax.set_ylabel("Height [m]")
+        ax.set_xlim(xlim_pc12)
+        ax.set_title("PC{}".format(i_pc+1))
+        ax.grid(True)
+        ax.set_xlabel(x_label)
+
+        prl, prp = get_profile(i_pc, 1, True)
+        ax.plot(prl, altitudes, label="Parallel", color='#ff7f0e')
+        ax.plot(prp, altitudes, label="Perpendicular", color='#1f77b4')
+
+        mag = np.sqrt((prl**2 + prp**2))
+        ax.plot(mag, altitudes, '--', label='Magnitude', ms=3, color='#2ca02c')
+
+        if not plots_interactive: plt.savefig(result_dir + 'pc_profile_{}'.format(i_pc+1) + plot_info + '.pdf')
+
+
 def plot_frequency_projection(data_pc, plot_info=""):
     plt.figure(figsize=(5, 2.5))
     plt.subplots_adjust(top=0.975, bottom=0.178, left=0.15, right=0.94)
@@ -133,11 +159,11 @@ def plot_frequency_projection(data_pc, plot_info=""):
 
 
 
-def analyse_pc(wind_data, loc_info=""):
+def analyse_pc(wind_data, loc_info="", n_pcs=5):
     altitudes = wind_data['altitude']
     normalized_data = wind_data['training_data']
     # Perform principal component analyis.
-    n_features = normalized_data.shape[1]
+    n_features = n_pcs #normalized_data.shape[1]
     pca = PCA(n_components=n_features)
     pipeline = pca
 
@@ -176,11 +202,140 @@ def analyse_pc(wind_data, loc_info=""):
     plot_mean_and_pc_profiles(altitudes, var, get_pc_profile, plot_info=loc_info)
 
 
+def plot_sample_profile_pc_sum(sample_profile, altitudes, get_profile, plot_info=""):
+    plot_n_pcs = 2
+    n_cols = 3
+    n_rows = plot_n_pcs
+    shape_map = (n_rows, n_cols)
+
+    x_label = r"$\tilde{v}$ [-]"
+    figsize = (8.4, 6)
+
+    fig, ax = plt.subplots(shape_map[0], shape_map[1], sharey=True, figsize=figsize)
+    wspace = 0.1
+    layout = {'top': 0.9, 'bottom': 0.085, 'left': 0.38, 'right': 0.985, 'hspace': 0.23}
+    plt.subplots_adjust(**layout, wspace=wspace)
+
+    # Add plot window for orginal profile
+    w, h, y0, x0 = ax[0, 0]._position.width, ax[0, 0]._position.height, ax[0, 0]._position.y0, ax[0, 0]._position.x0
+    ax_mean = fig.add_axes([x0-w*(1.5), y0, w, h])
+
+    prl, prp = sample_profile[:len(altitudes)], sample_profile[len(altitudes):]
+    ax_mean.plot(prl, altitudes, label="Parallel", color='#ff7f0e')
+    ax_mean.plot(prp, altitudes, label="Perpendicular", color='#1f77b4')
+    ax_mean.plot((prl**2 + prp**2)**.5, altitudes, '--', label='Magnitude', ms=3, color='#2ca02c')
+    ax_mean.set_title("Sample Profile")
+    ax_mean.grid(True)
+    ax_mean.set_ylabel("Height [m]")
+    ax_mean.set_xlim(x_lim_profiles)
+    ax_mean.set_xlabel(x_label)
+    ax_mean.legend(bbox_to_anchor=(1., 1.16, 3, 0.2), loc="lower left", mode="expand",
+                   borderaxespad=0, ncol=4)    
+    
+    # Plot PCs and PC multiplicands superimposed on the mean.
+    marker_counter = 0
+    i_plot_step = 0
+    for i_pc in range(plot_n_pcs):  # For every PC/row in the plot.
+        ax[i_pc, 0].set_ylabel("Height [m]")
+        for i_col in range(n_cols):
+            # Get profile data.
+            if i_col == 0 and i_pc == 0:
+                # plot mean profile
+                prl, prp = get_profile()
+                ax[i_pc, i_col].set_xlim(x_lim_profiles)
+                ax[i_pc, i_col].set_title("Mean")
+            else:  # Plots showing PC contributions superimposed on the mean.
+                i_plot_step += 1
+                prl, prp = get_profile(-2, sum_step=i_plot_step)
+                ax[i_pc, i_col].set_xlim(x_lim_profiles)
+                if i_plot_step > 2:
+                    ax[i_pc, i_col].set_title("Mean+PC1$\dots$PC{}".format(i_plot_step))
+                elif i_plot_step == 2:
+                    ax[i_pc, i_col].set_title("Mean+PC1+PC2")
+                else:
+                    ax[i_pc, i_col].set_title("Mean+PC{}".format(i_plot_step))
+
+            # Plot profiles.
+            # Plot magnitude 
+            ax[i_pc, i_col].plot(prl, altitudes, label="Parallel", color='#ff7f0e')
+            ax[i_pc, i_col].plot(prp, altitudes, label="Perpendicular", color='#1f77b4')
+
+            mag = np.sqrt((prl**2 + prp**2))
+            ax[i_pc, i_col].plot(mag, altitudes, '--', label='Magnitude', ms=3, color='#2ca02c')
+
+            marker_counter += 1
+            ax[i_pc, i_col].plot(0.1, 0.1, 's', mfc="white", alpha=1, ms=12, mec='k', transform=ax[i_pc, i_col].transAxes)
+            ax[i_pc, i_col].plot(0.1, 0.1, marker='${}$'.format(marker_counter), alpha=1, ms=7,
+                                 mec='k', transform=ax[i_pc, i_col].transAxes)
+            ax[i_pc, i_col].grid(True)
+
+    # Add labels on x-axes.
+    for i_col in range(shape_map[1]):
+            ax[-1, i_col].set_xlabel(x_label)
+    if not plots_interactive: plt.savefig(result_dir + 'pc_sample_mean_and_pc_contrib' + plot_info + '.pdf')
+
+
+
+def sample_profile_pc_sum(wind_data, loc_info="", n_pcs=5, i_sample=0):
+    altitudes = wind_data['altitude']
+    normalized_data = wind_data['training_data']
+    # Perform principal component analyis.
+    n_features = n_pcs #normalized_data.shape[1]
+    pca = PCA(n_components=n_features)
+    pipeline = pca
+
+    # print("{} features reduced to {} components.".format(n_features, n_components))
+    data_pc = pipeline.fit_transform(normalized_data)
+
+    # Plot results.
+    def get_pc_profile(i_pc=-1, multiplier=1., plot_pc=False, sum_step=1):
+        # Determine profile data by transforming data in PC to original coordinate system.
+        if i_pc == -1:
+            mean_profile = pipeline.inverse_transform(np.zeros(n_features))
+            profile = mean_profile
+        elif i_pc == -2:
+            # sequential sum of pca profile
+            sample_pca_profile_factors = data_pc[i_sample,:]
+            profile_cmp = np.zeros(n_features)
+            for i in range(sum_step):
+                profile_cmp[i] = sample_pca_profile_factors[i]
+            profile = pipeline.inverse_transform(profile_cmp)
+        else:
+            profile_cmp = np.zeros(n_features)
+            profile_cmp[i_pc] = multiplier
+            profile = pipeline.inverse_transform(profile_cmp)
+            if plot_pc:
+                mean_profile = pipeline.inverse_transform(np.zeros(n_features))
+                profile -= mean_profile
+        prl = profile[:len(altitudes)]
+        prp = profile[len(altitudes):]
+        return prl, prp
+
+    plot_pc_profiles(altitudes, get_pc_profile, plot_info=loc_info)
+    plot_sample_profile_pc_sum(normalized_data[i_sample,:], altitudes, get_pc_profile, plot_info=loc_info)
+
+
+
 if __name__ == '__main__':
+    import time
+    since = time.time()
+    
     wind_data = get_wind_data()
+    
+    time_elapsed = time.time() - since
+    print('Input read - Time lapsed: ', '\t{:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+
     from preprocess_data import preprocess_data
     wind_data = preprocess_data(wind_data)
+
+    time_elapsed = time.time() - since
+    print('Preprocessing done - Time lapsed: ', '\t{:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+
     # Run principal component analysis
-    analyse_pc(wind_data, loc_info=data_info)
+    analyse_pc(wind_data, loc_info=data_info, n_pcs=n_pcs)
+    sample_profile_pc_sum(wind_data, i_sample=100, loc_info=data_info, n_pcs=n_pcs)
+
+    time_elapsed = time.time() - since
+    print('PCA analysis done - Time lapsed: ', '\t{:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
 
     if plots_interactive: plt.show()
